3e45984adba3afefdd3ffe748240352a1ef2cd59
binaya-batas <batas.binaya@gmail.com>
2023-07-17 07:40:06 +0545

    Created routes for different pages.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
-import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import Home from './pages/Home';
+import About from './pages/About';
+import Store from './pages/Store';
+import Navbar from './components/Navbar';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-   <h1>Hello</h1>
+    <>
+      <Navbar />
+      <Container className="mb-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Container>
+    </>
   )
 }
 
