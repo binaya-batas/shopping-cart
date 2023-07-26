b104a798b580046a29a710e8d4854bb8e2082709
binaya-batas <batas.binaya@gmail.com>
2023-07-26 21:58:45 +0545

    Add shopping cart component  open and close functionality.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,10 @@
 import { Button, Container, Nav, Navbar as NavbarBs } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
+import { useShoppingCart } from "../context/ShoppingCartContext";
 
 const Navbar = () => {
+    const { openCart, cartQuantity } = useShoppingCart();
+
   return (
     <NavbarBs sticky="top" className="bg-white shadow-sm mb-3">
         <Container>
@@ -20,6 +23,7 @@ const Navbar = () => {
                 style={{ width: "3rem", height: "3rem", position: "relative" }}
                 variant="outline-primary"
                 className="rounded-circle"
+                onClick={() => openCart()}
             >
                 S
                 <div 
@@ -33,7 +37,7 @@ const Navbar = () => {
                         right: 0, 
                         transform: "translate(25%, 25%)"}}
                 >
-                    1
+                    {cartQuantity}
                 </div>
             </Button>
         </Container>
--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,4 +1,5 @@
 import { Button, Card } from "react-bootstrap";
+import { useShoppingCart } from "../context/ShoppingCartContext";
 import { formatCurrency } from "../utilities/formatCurrency";
 
 type StoreItemProps = {
@@ -9,7 +10,14 @@ type StoreItemProps = {
 };
 
 const StoreItem = ({ id, name, price, imgUrl }: StoreItemProps) => {
-  const quantity = 0;
+    const {
+        getItemQuantity,
+        increaseCartQuantity,
+        decreaseCartQuantity,
+        removeFromCart
+    } = useShoppingCart();
+
+    const quantity = getItemQuantity(id);
 
   return (
     <Card className="h-100">
@@ -25,15 +33,15 @@ const StoreItem = ({ id, name, price, imgUrl }: StoreItemProps) => {
           <span className="ms-2 text-muted">{formatCurrency(price)}</span>
         </Card.Title>
         <div className="mt-auto">
-          {quantity === 0 ? <Button>Add To Cart</Button> 
+          {quantity === 0 ? <Button onClick={() => increaseCartQuantity(id)}>Add To Cart</Button> 
           : 
           <div className="d-flex align-items-center flex-column" style={{ gap: ".5rem"}}>
             <div className="d-flex align-items-center flex-column" style={{ gap: ".5rem"}}>
-                <Button>-</Button>
-                <span className="fs-3">{quantity}</span> in cart
-                <Button>+</Button>
+                <Button onClick={() => decreaseCartQuantity(id)}>-</Button>
+                    <span className="fs-3">{quantity}</span> in cart
+                <Button onClick={() => increaseCartQuantity(id)}>+</Button>
             </div>
-            <Button variant="danger" size="sm">
+            <Button variant="danger" size="sm" onClick={() => removeFromCart(id)}>
                 Remove
             </Button>  
           </div> }
